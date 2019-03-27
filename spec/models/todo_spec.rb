135f096d6843b5ce4a7ad2fc5ed1875d640feb10
shimamchan <shimamuko.akane@tesyio.com>
2019-03-28 01:25:36 +0900

    Update model class files

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
   # Association test
-  it { should hame_many(:items).dependent(:destroy) }
+  it { should have_many(:items).dependent(:destroy) }
   # Validation tests
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:created_by) }
