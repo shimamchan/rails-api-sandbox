815372cba7613d1d1133dcd395f3f2fcb3de604f
shimamchan <shimamuko.akane@tesyio.com>
2019-03-31 21:25:16 +0900

    Add Authorization api request

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,6 +72,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   # add RequestSpecHelper
   config.include RequestSpecHelper, type: :request
 
