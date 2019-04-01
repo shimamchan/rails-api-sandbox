744b0091d6f00587568ed25cd9b9019c95b2f7d7
shimamchan <shimamuko.akane@tesyio.com>
2019-04-01 23:11:12 +0900

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  scope module: :v2, constraints: ApiVerision.new('v2') do
+  scope module: :v2, constraints: ApiVersion.new('v2') do
     resources :todos, only: :index
   end
   
